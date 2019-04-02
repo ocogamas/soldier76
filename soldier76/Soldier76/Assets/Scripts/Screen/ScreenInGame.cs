42dc4a89ebad77aa2c005f6610d4ab9b1fc7c209
masato kondo <kondo_masato@craftegg.co.jp>
2019-04-02 19:29:38 +0900

    ゲームのマス目　わからんなりに適当に作った。カメラとか空間とか勉強しないとさっぱりわからん

---
+++ soldier76/Soldier76/Assets/Scripts/Screen/ScreenInGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenInGame : MonoBehaviour
+{
+    private readonly uint TILE_X = 10;
+    private readonly uint TILE_Y = 16;
+
+    [SerializeField] private GameObject tilePrefab;
+    [SerializeField] private GameObject tilesGameObject;
+
+    void Start()
+    {
+        for (int x = 0; x < TILE_X; x++)
+        {
+            for (int y = 0; y < TILE_Y; y++)
+            {
+                GameObject obj = Instantiate(this.tilePrefab);
+
+                obj.transform.parent = this.tilesGameObject.transform;
+
+                obj.transform.localPosition = new Vector3(x*0.5f, y*0.5f, 0);
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
