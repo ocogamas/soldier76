bcd9a7049ba22e7282f66f6fc2e17400d08e9e22
masato kondo <kondo_masato@craftegg.co.jp>
2019-04-16 19:50:00 +0900

    DrumObject追加

---
+++ soldier76/Soldier76/Assets/Scripts/RhythmGame/Objects/DrumObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumObject : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private System.Action onTouchDown;
+
+    public void RegisterCallbackOnTouchDown(System.Action onTouchDown)
+    {
+        this.onTouchDown = onTouchDown;
+    }
+
+    public void OnTouchDown()
+    {
+        Debug.Log_yellow("OnTouchDown");
+        this.onTouchDown?.Invoke();
+        
+        this.animator.Play("DrumAnimation", 0, 0);
+    }
+
+
+}
