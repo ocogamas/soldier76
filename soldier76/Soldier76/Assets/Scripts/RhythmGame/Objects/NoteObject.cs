778bcf34e10bd3c06405120b481812863560b57b
masato kondo <kondo_masato@craftegg.co.jp>
2019-06-19 09:56:24 +0900

    enumとnote基本object追加

---
+++ soldier76/Soldier76/Assets/Scripts/RhythmGame/Objects/NoteObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+    // 担当するボタン
+    private DrumObject drumObject;
+
+    private NoteState noteState;
+
+
+    #region Public
+
+    public void Setup()
+    {
+        this.noteState = NoteState.Inactive;
+
+
+    }
+
+    public void ExecUpdate()
+    {
+        
+    }
+
+    #endregion // Public
+
+
+
+}
