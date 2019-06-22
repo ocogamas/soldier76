c313f0d63d314a2c0952126f75572547d33f0028
ocogamas <ocogamas@gmail.com>
2019-06-22 15:50:15 +0900

    Noteの準備

---
+++ soldier76/Soldier76/Assets/Scripts/RhythmGame/GameManager/NoteManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+	[SerializeField] private GameObject noteRoot;
+	[SerializeField] private GameObject[] noteObjectPositions;
+	
+
+	private void Awake()
+	{
+	
+	}
+	
+	
+	#region Public
+	
+	public void Setup()		
+	{
+		
+	}
+	
+	#endregion // Public
+	
+}
